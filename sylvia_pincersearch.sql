f70c44a261caa9c14d4687e0ee667a1319333cd1
Fahrul <fahrultech@gmail.com>
2020-03-09 10:33:16 +0700

    another mysql file

--- sylvia_pincersearch.sql
+++ sylvia_pincersearch.sql
@@ -1,75 +1,141 @@
-﻿# Host: localhost  (Version 5.5.5-10.4.8-MariaDB)
-# Date: 2020-03-09 10:15:28
-# Generator: MySQL-Front 6.0  (Build 2.20)
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 09 Mar 2020 pada 04.30
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-#
-# Structure for table "admin"
-#
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS `admin`;
+--
+-- Database: `sylvia_pincersearch`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
 CREATE TABLE `admin` (
-  `idAdmin` int(11) NOT NULL AUTO_INCREMENT,
+  `idAdmin` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`idAdmin`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-#
-# Data for table "admin"
-#
+--
+-- Dumping data untuk tabel `admin`
+--
 
-INSERT INTO `admin` VALUES (1,'Admin','admin','21232f297a57a5a743894a0e4a801fc3');
+INSERT INTO `admin` (`idAdmin`, `nama`, `username`, `password`) VALUES
+(1, 'Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
-#
-# Structure for table "barang"
-#
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `barang`;
+--
+-- Struktur dari tabel `barang`
+--
+
 CREATE TABLE `barang` (
-  `idBarang` int(11) NOT NULL AUTO_INCREMENT,
+  `idBarang` int(11) NOT NULL,
   `kodeBarang` varchar(255) NOT NULL,
-  `namaBarang` varchar(255) NOT NULL,
-  PRIMARY KEY (`idBarang`),
-  UNIQUE KEY `kodeBarang` (`kodeBarang`)
+  `namaBarang` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-#
-# Data for table "barang"
-#
+-- --------------------------------------------------------
 
+--
+-- Struktur dari tabel `detailtransaksi`
+--
 
-#
-# Structure for table "detailtransaksi"
-#
-
-DROP TABLE IF EXISTS `detailtransaksi`;
 CREATE TABLE `detailtransaksi` (
-  `idDetailTransaksi` int(11) NOT NULL AUTO_INCREMENT,
+  `idDetailTransaksi` int(11) NOT NULL,
   `noInvoice` varchar(255) NOT NULL DEFAULT '0',
-  `kodeBarang` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`idDetailTransaksi`)
+  `kodeBarang` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-#
-# Data for table "detailtransaksi"
-#
+-- --------------------------------------------------------
 
+--
+-- Struktur dari tabel `transaksi`
+--
 
-#
-# Structure for table "transaksi"
-#
-
-DROP TABLE IF EXISTS `transaksi`;
 CREATE TABLE `transaksi` (
-  `idTransaksi` int(11) NOT NULL AUTO_INCREMENT,
+  `idTransaksi` int(11) NOT NULL,
   `noInvoice` varchar(255) NOT NULL,
-  `tanggal` date DEFAULT NULL,
-  PRIMARY KEY (`idTransaksi`)
+  `tanggal` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-#
-# Data for table "transaksi"
-#
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`idAdmin`);
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`idBarang`),
+  ADD UNIQUE KEY `kodeBarang` (`kodeBarang`);
+
+--
+-- Indeks untuk tabel `detailtransaksi`
+--
+ALTER TABLE `detailtransaksi`
+  ADD PRIMARY KEY (`idDetailTransaksi`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`idTransaksi`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `idAdmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `idBarang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `detailtransaksi`
+--
+ALTER TABLE `detailtransaksi`
+  MODIFY `idDetailTransaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `idTransaksi` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
