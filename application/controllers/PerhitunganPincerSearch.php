5a927f848bcc8b83d9e2ed0a0429e8986b5aafa0
Fahrul <fahrultech@gmail.com>
2020-06-29 18:41:59 +0700

    fix double mfcs

--- application/controllers/PerhitunganPincerSearch.php
+++ application/controllers/PerhitunganPincerSearch.php
@@ -90,8 +90,8 @@ class PerhitunganPincerSearch extends CI_Controller{
         $no=0;
         foreach($dataDetailTransaksi as $ddt){
           $row = array();
-          $row["noInvoice"] = $ddt->noInvoice;
-          $row["kodeBarang"] = [$ddt->kodeBarang];
+          $row["noInvoice"] = trim($ddt->noInvoice);
+          $row["kodeBarang"] = [trim($ddt->kodeBarang)];
           $idx = array_search($ddt->noInvoice,array_column($detailTransaksiArray,'noInvoice')); 
           if($idx === false){
             $detailTransaksiArray[] = $row;
@@ -154,7 +154,7 @@ class PerhitunganPincerSearch extends CI_Controller{
         $pass["mfs"] = count($mfs) !== 0 ? $this->ss($mfs) : $mfs;
         $tempFreq = $frequent;
         count($mfs) > 0 ? $frequent = $this->lprune($frequent,$mfs) : $frequent = $frequent;
-        count($infrequent) !== 0 ? $mfcs = $this->removeMFCSSubset($this->sortByCount($this->getMFCS($infrequent,$mfcs))) : $mfcs = $mfcs;
+        $mfcs = $this->removeMFCSSubset($this->sortByCount($this->getMFCS($infrequent,$mfcs)));
         $pass["mfcs"] = count($mfcs) !== 0 ? $this->ss($mfcs) : $mfcs;
         $cdata = $this->removeMFCSSubset($this->joinData($frequent));
         if(count($tempFreq) !== count($frequent)){
